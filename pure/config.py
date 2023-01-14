dd23d94359a10c55af12acfb86a673005da61b31
Sumantha <sumanthakukkeka@gmail.com>
2023-01-14 18:58:50 +0530

    Announcement done

--- pure/config.py
+++ pure/config.py
@@ -3,7 +3,7 @@ class Config:
     MONGODB_DATABASE_URI = "mongodb://localhost:27017"
     MYSQL_DATABASE_URI = "mysql+mysqlconnector://localhost:3307/student_sanctuary"
     MYSQL_DATABASE_HOST = "localhost"
-    MYSQL_DATABASE_PORT = "3306"
+    MYSQL_DATABASE_PORT = "3307"
     MYSQL_DATABASE_USER = "root"
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
